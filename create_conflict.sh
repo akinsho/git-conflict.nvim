9fef42713111ada3bfc57663aff55ec572fe3790
Akin Sowemimo <akin.sowemimo@gmail.com>
2022-03-14 00:09:48 +0000

    chore: rename helper conflict file

--- create_conflict.sh
+++ create_conflict.sh
@@ -3,15 +3,15 @@
 mkdir git-repo
 cd git-repo
 git init
-touch test_code.lua
-git add test_code.lua
-echo "local value = 1 + 1" > test_code.lua
+touch conflicted.lua
+git add conflicted.lua
+echo "local value = 1 + 1" > conflicted.lua
 git commit -am 'initial'
 git checkout -b new_branch
-echo "local value = 1 - 1" > test_code.lua
+echo "local value = 1 - 1" > conflicted.lua
 git commit -am 'first commit on new_branch'
 git checkout main
-cat > test_code.lua<< EOF
+cat > conflicted.lua<< EOF
 local value = 5 + 7
 print(value)
 print(string.format("value is %d", value))
