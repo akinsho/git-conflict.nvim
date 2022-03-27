ca26a3b40318251476f7879d12ac01cafaa34ecd
wongxy <xiyao.wong@foxmail.com>
2022-03-27 02:58:19 -0700

    fix: use nvim_exec_autocmd (#5)
    Co-authored-by: Akin Sowemimo <akin.sowemimo@gmail.com>

--- lua/git-conflict.lua
+++ lua/git-conflict.lua
@@ -374,7 +374,10 @@ local function parse_buffer(bufnr, range_start, range_end)
     highlight_conflicts(positions, lines)
   end
   if prev_conflicts ~= has_conflict then
-    api.nvim_do_autocmd(
+    -- TODO: This should be removed in 2 weeks (i.e. early April 2022) once people
+    -- have had time to migrate to the new nightly
+    local exec_autocmd = api.nvim_do_autocmd or api.nvim_exec_autocmd
+    exec_autocmd(
       'User',
       { pattern = has_conflict and 'GitConflictDetected' or 'GitConflictResolved' }
     )
